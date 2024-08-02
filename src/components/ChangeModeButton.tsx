a5ad4ee1f3b201bfa0a3cc2339f0c8fab2813425
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2024-08-02 18:11:19 +0900

    戻す

--- src/components/ChangeModeButton.tsx
+++ src/components/ChangeModeButton.tsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import { RiMoonClearFill } from "react-icons/ri";
 import { PiSunHorizonBold } from "react-icons/pi";
-import Head from "next/head";
 
 export const ChangeModeButton = () => {
   const { theme, setTheme } = useTheme();
@@ -12,22 +11,14 @@ export const ChangeModeButton = () => {
   const handleOnClick = () => setTheme(theme === "light" ? "dark" : "light");
 
   return (
-    <>
-      <Head>
-        <meta
-          name="theme-color"
-          content={theme === "light" ? "#ffffff" : "#000000"}
-        />
-      </Head>
-      <Button
-        variant="ghost"
-        size="icon"
-        className="hover:bg-background/0 group relative"
-        onClick={handleOnClick}
-      >
-        <RiMoonClearFill className="block dark:hidden" size={24} />
-        <PiSunHorizonBold className="hidden dark:block" size={24} />
-      </Button>
-    </>
+    <Button
+      variant="ghost"
+      size="icon"
+      className="hover:bg-background/0 group relative"
+      onClick={handleOnClick}
+    >
+      <RiMoonClearFill className="block dark:hidden" size={24} />
+      <PiSunHorizonBold className="hidden dark:block" size={24} />
+    </Button>
   );
 };
