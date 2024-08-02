d21714638cc52352afcbb7b6626e07db1e1a055d
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2024-08-02 17:17:18 +0900

    なぞ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,12 +50,9 @@ export const metadata: Metadata = {
     description: APP_DESCRIPTION,
   },
 };
-// export const viewport: Viewport = {
-//   themeColor: [
-//     { media: "(prefers-color-scheme: light)", color: "white" },
-//     { media: "(prefers-color-scheme: dark)", color: "#000000" },
-//   ],
-// };
+export const viewport: Viewport = {
+  themeColor: "#ffffff",
+};
 
 export default function RootLayout({
   children,
