d8a32eeaa816d202b98045056cccb34443fb8143
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2025-12-12 17:06:42 +0900

    delete ragacy

--- legacy/src/app/practice/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Board } from "./_components/Board";
-
-export default function Home() {
-  return (
-    <div className="flex h-full items-center justify-center gap-4 py-6">
-      <Board />
-    </div>
-  );
-}
