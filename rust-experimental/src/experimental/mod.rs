fcfbd412d65b6737503d351bb8daf6aef050bc5d
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2026-01-09 16:55:46 +0900

    rust-experimentalをnightlyに

--- rust-experimental/src/experimental/mod.rs
+++ rust-experimental/src/experimental/mod.rs
@@ -1,12 +1,14 @@
-pub mod arena;
 pub mod core;
 pub mod holdem;
 pub mod simulated_icm;
+#[cfg(feature = "arena")]
+pub mod arena;
 
 pub fn run_all() {
   println!("== rs_poker 実験: モジュール別の機能デモ ==");
   core::run();
   holdem::run();
-  arena::run();
   simulated_icm::run();
+  #[cfg(feature = "arena")]
+  arena::run();
 }
