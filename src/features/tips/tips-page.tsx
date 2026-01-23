c3d190696d5c3d28c3ea3e993fd9f1bc5770e46d
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2026-01-23 15:09:19 +0900

    Update tips-page.tsx

--- src/features/tips/tips-page.tsx
+++ src/features/tips/tips-page.tsx
@@ -6,10 +6,7 @@ type TipsPageProps = HTMLAttributes<HTMLDivElement>;
 export const TipsPage = ({ className, ...props }: TipsPageProps) => {
   return (
     <div
-      className={cn(
-        "mx-auto flex min-h-dvh max-w-3xl flex-col gap-6 px-6 py-10",
-        className,
-      )}
+      className={cn("flex flex-col gap-6 px-6 py-10", className)}
       {...props}
     />
   );
